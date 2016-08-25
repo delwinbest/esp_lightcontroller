af57de0ee131ddfbbd8f1dde74e7e9e2dd91d6cf
Delwin <mail@delwinbest.com>
2016-08-25 14:49:32 +1000

    Initial Import
    aREST API works

---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+/*
+  This a simple example of the aREST Library for the ESP8266 WiFi chip.
+  See the README file for more details.
+
+  Written in 2015 by Marco Schwartz under a GPL license.
+*/
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <aREST.h>
+
+// Create aREST instance
+aREST rest = aREST();
+bool is_AP = true;
+
+
+// The port to listen for incoming TCP connections
+#define LISTEN_PORT           80
+
+// Create an instance of the server
+WiFiServer server(LISTEN_PORT);
+
+// Variables to be exposed to the API
+int temperature;
+int humidity;
+
+// Declare functions to be exposed to the API
+int ledControl(String command);
+
+
+// Either connect to AP or become AP
+void config_AP () {
+  if (is_AP){
+    // WiFi parameters
+    const char* ssid = "esp8266AP";
+    const char* password = "thereisnospoon";
+    // Setup WiFi network
+    WiFi.softAP(ssid, password);
+    Serial.println("");
+    Serial.println("WiFi created");
+
+    // Print the IP address
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+  } else {
+    // Connect to AP
+  }
+}
+
+
+
+void setup(void)
+{
+  // Start Serial
+  delay(1000);
+  Serial.begin(115200);
+
+  // Init variables and expose them to REST API
+  temperature = 24;
+  humidity = 40;
+  rest.variable("temperature",&temperature);
+  rest.variable("humidity",&humidity);
+
+  // Function to be exposed
+  rest.function("led",ledControl);
+
+  // Give name & ID to the device (ID should be 6 characters long)
+  rest.set_id("1");
+  rest.set_name("esp8266");
+  config_AP();
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+}
+
+void loop(){
+  // Handle REST calls
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  Serial.println("Client Connected. Waiting for Data:");
+
+  while (client.connected()) {
+    if (client.available()) {
+      rest.handle(client);
+      client.stop();
+    } // if (client.available())
+  } // while (client.connected())
+}
+
+
+
+// Custom function accessible by the API
+int ledControl(String command) {
+
+  // Get state from command
+  int state = command.toInt();
+
+  digitalWrite(6,state);
+  return 1;
+}
