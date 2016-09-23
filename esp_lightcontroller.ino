fc17424922e3cabb4871da15a0a429353944a541
Delwin <mail@delwinbest.com>
2016-09-23 22:46:38 +1000

    Fully functions
    All functions built out including EEPROM save/restore
    Todo:
    Build out manual button interface
    Send state to controller when changed using manual button

--- esp_lightcontroller.ino
+++ esp_lightcontroller.ino
@@ -1,7 +1,7 @@
 /*********************************************************************************************
  *  Purpose: ESP8266 device which binds to WiFi SSID and presents itself as a mySensor Device
  *  Current Design: ESP device is designed to ahve two NeoPixel LED strips attached
- *        Each Strip is explicitly referenced and ahrd coded, this is not OOP unfortunately
+ *        Each Strip is explicitly referenced and hard coded, this is not OOP unfortunately
  *        The initial design will focus on controlling one strip, but allow for the easy 
  *        addition of the second.
  *  Author: Delwin Best
@@ -21,8 +21,8 @@
 #define MY_BAUD_RATE 9600
 
 #define MY_GATEWAY_ESP8266
-#define MY_ESP8266_SSID "arduinowifi"
-#define MY_ESP8266_PASSWORD "thereisnospoon"
+#define MY_ESP8266_SSID "XXX"
+#define MY_ESP8266_PASSWORD "YYY"
 
 #define MY_ESP8266_HOSTNAME "esp8266_bedroom"
 
@@ -32,7 +32,7 @@
 
 #define MY_GATEWAY_MAX_CLIENTS 3
 
-#define MY_INCLUSION_MODE_FEATURE
+//#define MY_INCLUSION_MODE_FEATURE
 #define MY_INCLUSION_MODE_DURATION 60 
 #define MY_INCLUSION_MODE_BUTTON_PIN  3 
 
@@ -40,11 +40,8 @@
 #define MY_DEFAULT_RX_LED_PIN  BUILTIN_LED  // Receive led pin
 #define MY_DEFAULT_TX_LED_PIN  BUILTIN_LED  // the PCB, on board LED
 
-#if defined(MY_USE_UDP)
-  #include <WiFiUDP.h>
-#else
-  #include <ESP8266WiFi.h>
-#endif
+#include <ESP8266WiFi.h>
+
 
 //This section relates to the 'CHILD' sensors reporting to the repeater, they are presented independently.
 #define CHILD1_ID 1 
@@ -57,27 +54,31 @@
 #define LIGHT_OFF 0
 #define LIGHT_ON 1
 
-
 #include <MySensors.h>
 
-
-int CHILD1_STATE, CHILD1_DIMLEVEL, CHILD_ID;
+int CHILD1_STATE=1;
+int CHILD_ID;
+int CHILD1_DIMLEVEL=255;
+String CHILD1_COLOR="FFFFFF";
 int CHILDREN=2;
 int LastLightState=LIGHT_OFF;
 int LastDimValue=100; 
 
 MyMessage rgbMsg(CHILD1_ID, V_RGB);
 MyMessage dimMsg(CHILD1_ID+1, V_PERCENTAGE);
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(2, STRIP1_LEDPIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(STRIP1_NUMPIXELS, STRIP1_LEDPIN, NEO_GRB + NEO_KHZ800);
   
 void setup() {
   delay(1000);
   Serial.begin(9600);
   pixels.begin();
 
-  request( CHILD1_ID, V_RGB );
-  request( CHILD1_ID+1, V_PERCENTAGE );
+  //request( CHILD1_ID, V_RGB );
+  //request( CHILD1_ID+1, V_PERCENTAGE );
+  restore_Eeprom();
+  updateLEDStrip();
 
+  
 }
 
 // mySensor device presentation configuration
@@ -93,13 +94,15 @@ void presentation() {
   typedef enum {
     V_RGB, V_PERCENTAGE, V_WATT
   } variableType;
-  sleep(100);  
+  sleep(100);
+  return;  
 }
 
 
 void loop() {
 
 
+  
 }
 
 // main receive function for the mySensor receive handling
@@ -115,50 +118,101 @@ void receive(const MyMessage &message)
   if ((message.sensor == CHILD1_ID) || (message.sensor == CHILD1_ID+1)){
     if (message.type == V_LIGHT) {
       Serial.println( "V_LIGHT command received..." );
-      setBrightness(message.data);
+      handleVLIGHT(1, message.data);
     }
     if (message.type == V_RGB) {
       Serial.println( "V_RGB command received..." );
-      setColor(message.data);
+      CHILD1_COLOR=message.data;
+      setColor(CHILD1_COLOR);   // I still need to stop CHILD1_COLOR in eeprom and change to updateledstrip();
     }
     if (message.type == V_PERCENTAGE) {
       Serial.println( "V_PERCENTAGE command received..." );
-      setBrightness(message.data);
+      CHILD1_STATE=1;
+      CHILD1_DIMLEVEL = ((255/100) * atoi(message.data)); // Max is 255, upscale from percentage
+      updateLEDStrip();
     }
   }
+  return;
 }
 
+void handleVLIGHT(uint8_t CHILD, String data){
+  //Doing this for Child 1 for now
+  //Extend using CHILD variable
+  uint8_t state = data.toInt();
+  switch (state){
+    case 0:
+    {
+      // Switch Off
+      CHILD1_STATE=0; 
+      break;
+    }
+    case 1:
+    {
+      // Switch Off
+      CHILD1_STATE=1; 
+      break;
+    }
+    default: 
+      // if nothing else matches, do the default
+      // default is optional
+    break;
+  }
 
-void SendCurrentState2Controller()
-{
+  updateLEDStrip();
+  //updateController
+  return; 
+}
+
+void SendCurrentState2Controller(){
   if ((LastLightState==LIGHT_OFF)||(LastDimValue==0)) {
     send(rgbMsg.set(0));
   }
   else {
     send(rgbMsg.set("ffffff"));
   }
+  return;
 }
 
+//
+void updateLEDStrip(){
+  save_Eeprom();  
+  switch (CHILD1_STATE){
+    case 0:
+    {
+      //pixels.clear();
+      setColor("0");     //Setting this to 0 disabled the LED strip. will need to reinitialise 
+      break;
+    }
+    case 1:
+    {
+      pixels.begin();
+      setColor(CHILD1_COLOR);
+      setBrightness(CHILD1_DIMLEVEL);
+      break;
+    }
+    default: 
+      // if nothing else matches, do the default
+      // default is optional
+    break;
+  }
+  return;
+}
 
 // Custom function to set Brightness
-int setBrightness(String value) {
-  Serial.print("Setting Brightness to ");
-  Serial.println(value);
-  uint8_t brightness = value.toInt();
-  // Set Brightness to param value
-  if (brightness == 0){
-    setColor("0");
-  } else if (brightness == 1 ){
-    setColor("FFFFFF");
-  } else {
-    pixels.setBrightness(brightness);
-    pixels.show();
-  }
+void setBrightness(uint8_t brightness) {
+  Serial.print("Setting Brightness: ");
+  Serial.println(String(brightness));
+  //Serial.println(value);
+  //uint8_t brightness = value.toInt();
+  pixels.setBrightness(brightness);
+  pixels.show();
+  delay(100);
+  return; 
 }
 
 // Convert HTML color code into RGB values
 // and set strip
-int setColor(String value) {
+void setColor(String value) {
   Serial.print("Setting Color To: ");
   Serial.println(value);
   long number = (long) strtol( &value[0], NULL, 16);
@@ -170,9 +224,28 @@ int setColor(String value) {
   for(i=0; i<pixels.numPixels(); i++) {
     pixels.setPixelColor(i, red, green, blue);
   }
+  
   pixels.show();
-
+  delay(100);
+  return;
 }
 
+void save_Eeprom(){
+  saveState(EPROM_CHILD1_STATE, CHILD1_STATE);
+  saveState(EPROM_CHILD1_DIMLEVEL,CHILD1_DIMLEVEL);
+  // Save Color Array
+  for(int i=0; i<CHILD1_COLOR.length(); i++) { 
+    saveState(EPROM_CHILD1_COLOR+i,CHILD1_COLOR[i]);
+  }
+}
+
+void restore_Eeprom(){
+  CHILD1_STATE=loadState(EPROM_CHILD1_STATE);
+  CHILD1_DIMLEVEL=loadState(EPROM_CHILD1_DIMLEVEL);
+  // Save Color Array
+  for(int i=0; i<CHILD1_COLOR.length(); i++) { 
+    CHILD1_COLOR[i]=loadState(EPROM_CHILD1_COLOR+i);
+  }
+}
 
 
